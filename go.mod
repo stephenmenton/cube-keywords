a73410fa3bf1b6be188598427bb031cf5ad23ebf
aqua Menton <stephen.menton@gmail.com>
2021-09-13 22:37:06 -0700

    refactor: rewrite as AWS Lambda using s3 bucket of simplified Scryfall bulk-data

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/aws/aws-lambda-go v1.26.0
+	github.com/aws/aws-sdk-go v1.40.42
 	github.com/tidwall/gjson v1.6.8
 )
--- main.go
+++ main.go
@@ -6,119 +6,119 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"regexp"
 	"sort"
 	"strings"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/tidwall/gjson"
 )
 
 const (
 	cobraDownloadBase = "https://cubecobra.com/cube/download/plaintext/"
-	scryfallNamedURL  = "https://api.scryfall.com/cards/named"
 )
 
-func parseCube(cubeID string) (map[string]int, error) {
-	cards, err := getList(cubeID)
-	if err != nil {
-		return map[string]int{}, err
-	}
-
-	freq := make(map[string]int)
-
-	// for each card
-	for _, v := range cards {
-		if v != "" {
-			// get details
-			time.Sleep(20 * time.Millisecond) // don't kill scryfall
-			k, l, _ := getCardDetails(v)      // keywords, layout
-			if l != "normal" {
-				freq[strings.ToLower(l)]++
-			}
-			for _, kv := range k {
-				freq[strings.ToLower(kv)]++
-			}
-		}
-	}
-
-	return freq, nil
-}
-
-func getList(cubeID string) ([]string, error) {
-	u, _ := url.Parse(cobraDownloadBase)
-	u, _ = u.Parse(cubeID)
-
-	// get the list
-	resp, err := http.Get(u.String())
-	if err != nil || resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("error retrieving cube [%s]", cubeID)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	// bad list?
-	badCube, _ := regexp.Match(`<html`, body)
-	if badCube == true {
-		return nil, fmt.Errorf("invalid cube [%s]", cubeID)
-	}
-	cards := strings.Split(string(body), "\n")
-	// strip last one, empty
-	if cards[len(cards)-1] == "" {
-		cards = cards[:len(cards)-1]
-	}
-
-	return cards, nil
-}
-
-// returns slice of keywords, layout, err
-func getCardDetails(name string) ([]string, string, error) {
-	u, _ := url.Parse(scryfallNamedURL)
-	uv := url.Values{}
-	uv.Add("exact", name)
-	u.RawQuery = uv.Encode()
-
-	// get the card
-	resp, err := http.Get(u.String())
-	if err != nil || resp.StatusCode != http.StatusOK {
-		return nil, "", fmt.Errorf("error retrieving card [%s]", name)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	gk := gjson.GetBytes(body, "keywords")
-	var keywords []string
-	for _, v := range gk.Array() {
-		keywords = append(keywords, v.Str)
-	}
-	gl := gjson.GetBytes(body, "layout")
-	return keywords, gl.Str, nil
-}
-
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	pc, found := request.PathParameters["cube"]
+	cubeID, found := request.PathParameters["cube"]
 	if found {
-		// path parameters are typically URL encoded so to get the value
-		cubeID, err := url.QueryUnescape(pc)
+		// download cube list
+		u, _ := url.Parse(cobraDownloadBase)
+		u, _ = u.Parse(cubeID)
+
+		// get the list
+		resp, err := http.Get(u.String())
+		if err != nil || resp.StatusCode != http.StatusOK {
+			return events.APIGatewayProxyResponse{}, fmt.Errorf("error retrieving cube [%s]", cubeID)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		// failure to read
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
+		// bad list?
+		badCube, _ := regexp.Match(`<html`, body)
+		if badCube {
+			return events.APIGatewayProxyResponse{}, fmt.Errorf("invalid cube [%s]", cubeID)
+		}
+		cards := strings.Split(string(body), "\r\n")
+		// strip last one, empty
+		if cards[len(cards)-1] == "" {
+			cards = cards[:len(cards)-1]
+		}
+		// now have cards populated with cube list
+
+		// download oracle
+		bucket := "cube-keywords"
+		item := "oracle-cards.json"
+
+		sess, _ := session.NewSession(&aws.Config{
+			Region: aws.String("us-west-2")},
+		)
+
+		file, err := os.Create("/tmp/" + item)
 		if err != nil {
 			return events.APIGatewayProxyResponse{}, err
 		}
 
-		freq, err := parseCube(cubeID)
+		defer file.Close()
+
+		downloader := s3manager.NewDownloader(sess)
+
+		_, err = downloader.Download(file,
+			&s3.GetObjectInput{
+				Bucket: aws.String(bucket),
+				Key:    aws.String(item),
+			})
 		if err != nil {
 			return events.APIGatewayProxyResponse{}, err
 		}
+		oracleBytes, err := ioutil.ReadFile("/tmp/" + item)
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
+		oracle := string(oracleBytes)
+		if oracle != "" {
+		}
 
-		var body string
+		// find keywords
+		// frequency of keyword/layout
+		freq := make(map[string]int)
+
+		// for each card...
+		for _, v := range cards {
+			// do nothing
+			if v != "" {
+			}
+			// get keywords
+			keywords := gjson.Get(oracle, fmt.Sprintf(`#(name="%s").keywords`, v))
+			if keywords.Exists() {
+				// return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%+v", keywords), StatusCode: 200, Headers: map[string]string{"Content-Type": "text/html; charset=UTF-8"}}, nil
+				keywords.ForEach(func(key, value gjson.Result) bool {
+					freq[strings.ToLower(value.String())]++
+					return true // keep iterating
+				})
+			}
+		}
+
+		var returnBody string
 		var keys []string
 		for k := range freq {
 			keys = append(keys, k)
 		}
 		sort.Strings(keys)
 		for _, k := range keys {
-			body += fmt.Sprintf("\"%s\" %d\n", k, freq[k])
+			returnBody += fmt.Sprintf("\"%s\" %d\n", k, freq[k])
 		}
-		return events.APIGatewayProxyResponse{Body: body, StatusCode: 200, Headers: map[string]string{"Content-Type": "text/html; charset=UTF-8"}}, nil
+
+		return events.APIGatewayProxyResponse{Body: returnBody, StatusCode: 200, Headers: map[string]string{"Content-Type": "text/html; charset=UTF-8"}}, nil
+
 	} else {
 		return events.APIGatewayProxyResponse{}, fmt.Errorf("unable to determine cubeID")
 	}
