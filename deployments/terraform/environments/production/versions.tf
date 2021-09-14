7af5d4c45bd82a89396a23e54809554434c63af4
aqua Menton <stephen.menton@gmail.com>
2021-09-14 04:09:58 -0700

    feat: use s3 backend for Terraform remote state

--- deployments/terraform/environments/production/versions.tf
+++ deployments/terraform/environments/production/versions.tf
@@ -5,4 +5,9 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-dad6179d51d50997fb57c92d"
+    key    = "stephenmenton/cube_keywords"
+    region = "us-west-2"
+  }
 }
